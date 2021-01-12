37defffbe8768e7d0e3abcadd895a76be18144c8
Fredpwol <cyborg290@gmail.com>
2021-01-12 20:14:38 +0100

    smart update thumbnail update

--- content.js
+++ content.js
@@ -1,7 +1,27 @@
+// use this variable to track the length of videos in case of list updates.
+var visitedThumbnails = new Set();
+var visitedThumbnailLinks = new Set();
+var lastThumbnailLength = 0;
+
 setInterval(function (){
-  document.querySelectorAll("a#thumbnail yt-img-shadow img").forEach(image =>{
-    image.style.filter = "blur(10px)"
-})
+  var thumbnails = document.querySelectorAll("a#thumbnail yt-img-shadow img");
+  var thumbnailLinks = document.querySelectorAll("a#thumbnail");
+
+    thumbnails.forEach((image, key) =>{
+      if ((!visitedThumbnails.has(key)) || key === lastThumbnailLength){
+        chrome.storage.local.get("blurRange", result => {
+          image.style.filter = `blur(${result.blurRange}px)`
+        })
+        visitedThumbnails.add(key);
+      }
+    })
+    lastThumbnailLength = thumbnails.length-1;
+    thumbnailLinks.forEach((link, key) => {
+      if(! visitedThumbnailLinks.has(key)){
+        parseLink(link.href);
+        visitedThumbnailLinks.add(key);
+      }
+    })
 }, 3000)
 
 
