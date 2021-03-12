8550ce9c6f89a20e9a0a47cab16b236021b91df0
Fredpwol <cyborg290@gmail.com>
2021-03-12 20:30:18 +0100

    improved blacklist functionalties

--- content.js
+++ content.js
@@ -6,27 +6,44 @@ var blurRange = 0; //tracks if the blur value changes.
 var videoClip = null;
 var updatedThumbnails = new Set(); // use this to check if the video thumbnail has been updated
 var thumbnailClip = "";
+var previousList = [];
+
+function isEqaul(firstArr, secondArr) {
+  if (firstArr.length !== secondArr.length) return false;
+  firstArr.sort()
+  secondArr.sort()
+  for (let index = 0; index < firstArr.length; index++) {
+    if (firstArr[index] != secondArr[index]) return false;
+  }
+  return true;
+}
 
 setInterval(function () {
   var thumbnailLinks = document.querySelectorAll("a#thumbnail");
   chrome.storage.local.get("videoClip", (res) => {
     chrome.storage.local.get("blurRange", (result) => {
       chrome.storage.local.get("thumbnailRange", (thumbnailRangeStore) => {
+        chrome.storage.local.get("blacklist", (storage) => {
+        const channels = storage.blacklist.map(channel => channel.toLowerCase());
         thumbnailLinks.forEach((link, key) => {
           if (
             (!visitedThumbnails.has(key) ||
               (blurRange != result.blurRange) ||
               (videoClip != res.videoClip) ||
               !updatedThumbnails.has(key) ||
+              !isEqaul(channels, previousList) ||
               (thumbnailRangeStore.thumbnailRange != thumbnailClip)) &&
             (key != thumbnailLinks.length - 1)
           ) {
             {
               const image = link.querySelector("yt-img-shadow img");
-              const channelName = link.parentElement.nextElementSibling.querySelector("a#avatar-link").title.toLowerCase();
-              chrome.storage.local.get("blacklist", (storage) => {
-                const channels = storage.blacklist.split(",").map(channel => channel.toLowerCase());
-                if (!channels.includes(channelName)){
+              let channelName = link.parentElement.nextElementSibling.querySelector("a#avatar-link")?.title.toLowerCase();
+              // console.log("channel name", channelName)
+              if (!channelName) {
+                let name = link.parentElement.nextElementSibling.querySelector("#channel-info")?.querySelector("yt-formatted-string a").innerHTML;
+                channelName = name.toLowerCase();
+              }
+                if (channels.includes(channelName)){
                   if (!res.videoClip) {
                     if (!image.src.match("https://i.ytimg.com/vi/.*/(hqdefault|mqdefault|hq720).jpg?.*")){
                       updateThumbnails("hq720",image)
@@ -48,22 +65,26 @@ setInterval(function () {
                         default:
                           clip = "hq1";
                       }
-                      console.log(clip, thumbnailRangeStore.thumbnailRange)
                       updateThumbnails(clip, image);
                       updatedThumbnails.add(key);
                     }
                   }
                 }
-              })
-              
+                else if(previousList.includes(channelName))
+                {
+                  updateThumbnails("hq720",image);
+                  image.style.filter = "blur(0px)"
+                }
+               
             }
-            parseLink(link.href)
             visitedThumbnails.add(key);
           }
+          })
+          previousList = channels; // use this to keep track of elements removed from the blacklist.
+          videoClip = res.videoClip;
+          thumbnailClip = thumbnailRangeStore.thumbnailRange;
+          blurRange = result.blurRange;
         });
-        videoClip = res.videoClip;
-        thumbnailClip = thumbnailRangeStore.thumbnailRange;
-        blurRange = result.blurRange;
       });
     });
   });
